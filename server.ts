ea60b539933a1d0e3a16191774bbe76c41b2031b
fravaud <florent.ravaud@atinternet.com>
2017-10-09 13:53:14 +0200

    update

--- server.ts
+++ server.ts
@@ -35,8 +35,9 @@ connectionGet.on('ready', function () {
         // Receive messages
         q.subscribe(function (message) {
             // Print messages to stdout
-            console.log("Worker msg:" + JSON.stringify(message.uuid));
-            connectionPush.publish(message.uuid, {msg: "Traiter uuid: " + message.uuid}, (param1, param2) => {
+            console.log("Worker Receive msg:" + JSON.stringify(message.uuid));
+            connectionPush.publish(message.uuid, {msg: "Traiter uuid: " + message.uuid}, { persistent: true }, (param1, param2) => {
+                console.log("Worker Send msg:" + JSON.stringify({msg: "Traiter uuid: " + message.uuid}));
                 console.log("param1 : " + param1);
                 console.log("param2 : " + param1);
             });
