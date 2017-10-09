9184b9531b87b627b2e4a7be1569f0a9a62ce60c
fravaud <florent.ravaud@atinternet.com>
2017-10-09 13:38:35 +0200

    2 connection

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var amqp = require("amqp");
-var connection = amqp.createConnection({
+var connectionGet = amqp.createConnection({
     host: 'rabbitserver',
     login: 'fravaud',
     password: 'BBjakmlc100489'
 });
 // add this for better debuging
-connection.on('error', function (e) {
+connectionGet.on('error', function (e) {
     console.log("Error from amqp: ", e);
 });
+var connectionPush = amqp.createConnection({
+    host: 'rabbitserver',
+    login: 'fravaud',
+    password: 'BBjakmlc100489'
+});
+// add this for better debuging
+connectionPush.on('error', function (e) {
+    console.log("Error from amqp: ", e);
+});
+connectionPush.on('ready', function () {
+    console.log('Connect to (rabbitserver) connectionPush');
+});
 // Wait for connection to become established.
-connection.on('ready', function () {
-    console.log('Connect to (rabbitserver)');
+connectionGet.on('ready', function () {
+    console.log('Connect to (rabbitserver) connectionGet');
     // Use the default 'amq.topic' exchange
-    connection.queue('worker', function (q) {
+    connectionGet.queue('worker', function (q) {
         // Catch all messages
         q.bind('#');
         // Receive messages
         q.subscribe(function (message) {
             // Print messages to stdout
             console.log("Worker msg:" + JSON.stringify(message.uuid));
-            connection.publish(message.uuid, { msg: "Traiter uuid: " + message.uuid });
+            connectionPush.publish(message.uuid, { msg: "Traiter uuid: " + message.uuid });
         });
     });
 });
